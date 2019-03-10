6270b0259eda4348cb16f386826c1b56fa6425d0
3ayazaya <dan97-10@mail.ru>
2019-03-10 19:05:35 +0300

    Make some changes

--- new_parser.py
+++ new_parser.py
@@ -1,27 +1,38 @@
-import csv
-import time
+
 from Parser import Parser
 
 def main():
-    base = {}
-
-    file_name = 'maillog.txt'
-    with open(file_name, 'r', encoding='utf-8',errors='ignore') as file:
-        file_lines = file.readlines()
-        for line in file_lines:
-            base.update({'Date' : Parser.ParseDate(line)}) 
-            base.update({'Application' : ''}) 
-            base.update({'Protocol' : ''})
-            base.update({'Port' : ''})
-            base.update({'Login' : Parser.ParseLogin(line)}) 
-            base.update({'IP' : ''})
-            base.update({'rip' : ''})
-            base.update({'lip' : ''})
-            base.update({'Country' : Parser.ParseCountry(line)})
-            base.update({'City' : Parser.ParseCity(line)})
-            base.update({'Session' : Parser.ParseSession(line)})
-            print(base)  
-                    
+    fieldNames = ['Date','Application','Protocol', 'Process', 'Port','Login', 'IP', 'rip', 'lip', 'Country', 'City', 'Session']
+   
+    fileName = 'maillog.txt'
+    with open(fileName, 'r', encoding='utf-8',errors='ignore') as file:
+        fileLines = file.readlines()
+    
+        with open("result.csv", "a", ) as outFile:
+            writer = csv.writer(outFile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow(fieldNames)
+            outFile.close()
+        
+        for line in fileLines:
+            raw = [Parser.ParseDate(line), 
+            Parser.ParseApplication(line), 
+            Parser.ParseProtocol(line), 
+            Parser.ParseProcess(line), 
+            Parser.ParsePort(line), 
+            Parser.ParseLogin(line),
+            Parser.ParseIP(line),
+            Parser.ParseIPrip(line),
+            Parser.ParseIPlip(line),
+            Parser.ParseCountry(line),
+            Parser.ParseCity(line), 
+            Parser.ParseSession(line)]
+            
+            with open("result.csv", "a") as outFile:
+                writer = csv.writer(outFile, delimiter=';', lineterminator='\n')
+                writer.writerow(raw)
+                
+        outFile.close()
+    file.close()        
 
 if __name__ == "__main__":
     main()
