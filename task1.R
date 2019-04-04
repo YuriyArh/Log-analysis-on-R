c61f9ce9e729d4575b63153bcd1125eb7652e609
Aleksander Petrovskii <petr0vskjy.aleksander@gmail.com>
2019-04-04 20:36:40 +0300

    извлекли и отсортировали по времени выборку с коннектами

--- task1.R
+++ task1.R
@@ -9,9 +9,11 @@
 rm(list=ls())
 library(tidyr)
 library(dplyr)
+
 library(stringr)
 library(ggplot2)
 library(lubridate)
+library(tcltk2)
 ######################### Functions() ###############################
 
 #------------ find_multiply_ip_for_login(login, d_frame)---------------
@@ -52,12 +54,15 @@ find_multiply_ip_for_login <- function(login, d_frame) {
   # create empty dataframe
   mult_logins_user <- data.frame()
   # find e-mail addresses that have more than three IP
+ 
+   pb <- txtProgressBar(min = 1, max = nrow(unique_logins_table), style = 3) 
   for (i in 1:nrow(unique_logins_table)) {
     number_of_ip <- find_multiply_ip_for_login(unique_logins_table$value[i], user_connection_table)
     e_mail <- as.character(unique_logins_table$value[i])
     if (number_of_ip > 2){
-      print(e_mail)
-      print(number_of_ip)
+      #print(e_mail)
+      #print(number_of_ip)
+      setTxtProgressBar(pb, i)
       # datatable with e-mail addresses and the number of unique ip addresses
       mult_logins_user <- rbind( mult_logins_user, data.frame(e_mail, number_of_ip))
     }
@@ -65,42 +70,34 @@ find_multiply_ip_for_login <- function(login, d_frame) {
   length(mult_logins_user$e_mail) # number of e-mail
   table(mult_logins_user)  # static table of uniq ip
   # create data.frame for analysis
+  pb <- txtProgressBar(min = 1, max = nrow(mult_logins_user), style = 3) 
   user_connection_table_ip <- data.frame()
   for (i in 1:nrow(mult_logins_user)) {
     user_connection_table_ip_temp <- filter(user_connection_table, Login == as.character(mult_logins_user$e_mail[i] ))
     user_connection_table_ip <- rbind( user_connection_table_ip, user_connection_table_ip_temp)
+    setTxtProgressBar(pb, i)
   }
-  # work with date 
-  # convert  user_connection_table_ip$Date to POSIXlt format
+  # work with date -------
+  # convert  user_connection_table_ip$Date to POSIXct format
+  user_connection_table_ip$Date <-ymd_hms(user_connection_table_ip$Date)
+  # and sort user_connection_table_ip by date-time
+  user_connection_table_ip  <-   arrange(user_connection_table_ip, Date)
   
-  user_connection_table_ip$Date <- as.character(user_connection_table_ip$Date) %>%
-      strptime(format = '%Y %b %d %H:%M:%S', 'GMT') 
-      #arrange(Date) 
   #-----------------------------------------------------------------------
   # fix the time window and move it along the frame user_connection_table_ip 
   # inside each step we will check the connection from at least 
   # three unique ip-addresses, if the conditions are met, 
   # we display the rows in the summary table  user_connection_table_final
   #-----------------------------------------------------------------------
-  user_connection_table_final <- data.frame()
-  #W <- length(user_connection_table_ip$Date)
-  
-  VVV <- order(user_connection_table_ip$Date)
-  
-  W = 10
-  for (i in 1:(W-1)) {
-  #print(user_connection_table_ip$Date[i])
-  #let`s calculate the delta by time
-    time.i <- user_connection_table_ip$Date[i] %>%
-      ymd_hms() 
-    time.i.next <- user_connection_table_ip$Date[i+1] %>%
-      ymd_hms() 
-   delta.t <- difftime(time.i.next, time.i, units="mins")
-   print(i)
-   print(delta.t)
-  }
   
  # debug-zone-------- 
+  start_time <- user_connection_table_ip$Date[1]
+  
+  user_connection_table_ip %>% 
+    filter(Date == start_time )
+  
+  
+  
   X1 <- user_connection_table_ip$Date[9] %>%
     ymd_hms()
   
@@ -120,4 +117,20 @@ find_multiply_ip_for_login <- function(login, d_frame) {
   
   Q <- difftime(X38,X37,units="mins")
   as.numeric(Q)  
+  
+  
+  
+    ## Create Time Sequence -
+    x <- timeSequence(from = "2001-01-01", to = "2009-01-01", by = "day")
+    
+    ## Generate Periods -
+    periods(x, "2m", "1m")
+    periods(x, "52w", "4w")
+    
+    ## Roll Periodically -
+    periodicallyRolling(x)
+    
+    ## Roll Monthly -
+    monthlyRolling(x)
+   
   
